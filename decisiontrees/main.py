234ee0c6cb596d3a76ecb6ed42b2e76d4bc8b1fa
Brian Sheldon <briansouthpas@gmail.com>
2025-03-05 16:54:51 -0800

    cleanup

--- decisiontrees/main.py
+++ decisiontrees/main.py
@@ -1,56 +1,69 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from utils import train_test_split
-from decision_tree import DecisionTree
-from random_forest import RandomForest
-
+from models.decision_tree import DecisionTree
+from models.random_forest import RandomForest
+from models.adaboost import AdaBoost
 # initialize data
 df = pd.read_csv("data/train.csv")
 
 # preprocess data
 df = df.drop(["PassengerId", "Cabin", "Ticket", "Name"], axis=1)
 df = df.dropna()
-
-# convert male/female to binary labels
 df["Sex"] = df["Sex"].map({"male": 0, "female": 1}).astype('int64')
 df["Embarked"] = df["Embarked"].map({"S": 0, "C": 1, "Q": 2}).astype('int64')
 
 # Construct X_train, X_test, y_train, y_test
 X = df.drop(columns=["Survived"]).to_numpy()
 y = df["Survived"].to_numpy()
-
-# Create training and testing data
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-df.head(n=40)
 
-dt = DecisionTree()
+# Train Decision Tree
+dt = DecisionTree(max_depth=5, min_leaf_size=10)
 dt.train(X_train, y_train)
-y_pred = dt.predict(X_test)
+dt_pred = dt.predict(X_test)
 
+
+# Analyze Decision Tree
+cm = confusion_matrix(y_test, dt_pred)
+precision = precision_score(y_test, dt_pred)
+recall = recall_score(y_test, dt_pred)
+f1 = f1_score(y_test, dt_pred)
+
+print("Decision Tree: ")
+print(cm)
+print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1-Score: {f1:.2f}\n\n")
+
+
+# Train Random Forest
 rf = RandomForest()
-rf.train(X_train, y_train, num_models=30)
+rf.train(X_train, y_train, num_models=10)
 rf_pred = rf.predict(X_test)
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-
-from sklearn.metrics import precision_score, recall_score, f1_score
-
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-
-print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1-Score: {f1:.2f}")
-
-print('')
-print('')
+# Analyze Random Forest
 cm = confusion_matrix(y_test, rf_pred)
-print(cm)
-
 precision = precision_score(y_test, rf_pred)
 recall = recall_score(y_test, rf_pred)
 f1 = f1_score(y_test, rf_pred)
 
+print("Random Forest: ")
+print(cm)
+print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1-Score: {f1:.2f}\n\n")
+
+
+# Train AdaBoost
+ada = AdaBoost()
+ada.train(X_train, y_train)
+ada_pred = ada.predict(X_test)
+
+# Analyze AdaBoost
+cm = confusion_matrix(y_test, ada_pred)
+precision = precision_score(y_test, ada_pred)
+recall = recall_score(y_test, ada_pred)
+f1 = f1_score(y_test, ada_pred)
+
+print("AdaBoost: ")
+print(cm)
 print(f"Precision: {precision:.2f}, Recall: {recall:.2f}, F1-Score: {f1:.2f}")
