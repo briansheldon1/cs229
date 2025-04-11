e2fb48d00cfd442d964b51b1ba77a0d527b577f2
Brian Sheldon <briansouthpas@gmail.com>
2025-04-11 14:51:47 -0700

    documentation

--- supervised/L1_norm_linreg/utils.py
+++ supervised/L1_norm_linreg/utils.py
@@ -1,17 +1,14 @@
 import numpy as np
 
 def load_data(root):
-    ''' Load data from .dat file '''
+    ''' load data from .dat file '''
     with open(root) as f:
-
-        # load data, split by spaces
+        # split by spaces
         data = [line.strip().split(" ") for line in f]
-
-        # remove empty strings while converting to float
+        # remove empty strings and convert to float
         data = [np.array([float(cell) for cell in row if cell]) for row in data]
-
     return np.squeeze(np.array(data))
 
-
 def compare_theta(theta_true, theta_pred):
-    return sum(abs(theta_pred - theta_true))/len(theta_true)
+    # mean absolute difference
+    return sum(abs(theta_pred - theta_true)) / len(theta_true)
